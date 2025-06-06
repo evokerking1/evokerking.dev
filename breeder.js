d4aa20863ee6031336ab892a82a77939b3ec5aa3
DedFishy <boynegregg312@gmail.com>
2025-06-05 21:59:15 -0400

    better

---
+++ breeder.js
@@ -0,0 +1,14 @@
+const disclaimer = document.getElementById("disclaimer");
+
+var string = "better.boyne.dev";
+var stringProgress = 0;
+
+disclaimer.onclick = (_) => {
+    disclaimer.className = "bigger-is-better";
+    if (stringProgress >= string.length) {
+        document.location.replace("https://better.boyne.dev")
+    }
+    stringProgress++;
+    disclaimer.innerText = string.slice(0, stringProgress);
+    
+}
